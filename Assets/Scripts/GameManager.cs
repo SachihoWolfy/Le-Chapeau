949ed1336ff26c6d3584d638ba5815277f7bbe31
Sachiterasu <teirishwolf@gmail.com>
2023-08-29 19:59:32 -0700

    More progress, but some heartache
    Things are starting to not make sense

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+
+    [Header("Stats")]
+    public bool gameEnded = false; // has the game ended?
+    public float timeToWin; // time a player needs to hold the hat to win
+    public float invincibleDuration; // how long after a player gets the hat, are they invincible
+    private float hatPickupTime; // the time the hat was picked up by the current holder
+    [Header("Players")]
+    public string playerPrefabLocation; // path in Resources folder to the Player prefab
+    public Transform[] spawnPoints; // array of all available spawn points
+    public PlayerController[] players; // array of all the players
+    public int playerWithHat; // id of the player with the hat
+    private int playersInGame; // number of players in the game
+                               // instance
+    public static GameManager instance;
+    void Awake()
+    {
+        // instance
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        photonView.RPC("ImInGame", RpcTarget.All);
+    }
+
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+    // spawns a player and initializes it
+    void SpawnPlayer()
+    {
+        // instantiate the player across the network
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[Random.Range(0, spawnPoints.Length)], Quaternion.identity);
+        // get the player script
+        PlayerController playerScript = playerObj.GetComponent<PlayerController>();
+        // initialize the player
+        playerScript.photonView.RPC("Initialize", RpcTargets.All, PhotonNetwork.LocalPlayer);
+    }
+    public PlayerController GetPlayer(int playerId)
+    {
+        return players.First(x => x.id == playerId);
+    }
+    public PlayerController GetPlayer(GameObject playerObject)
+    {
+        return players.First(x => x.gameObject == playerObject);
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
